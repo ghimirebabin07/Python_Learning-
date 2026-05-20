647800190050452052c89603fc99752eb186c4b6
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-05-20 21:34:38 +0545

    Json file concept

---
+++ Basic_python/Json.py
@@ -0,0 +1,58 @@
+import json 
+import os 
+
+class InsufficientFundsError(Exception):
+    def __init__(self,balance, amount):
+        super().__init__(f"Cannot withdraw Rs.{amount}. Available: Rs.{balance}")
+
+
+class NegativeAmountError(Exception):
+    def __init__(self):
+        super().__init__("Amount must be greater than zero.")
+
+
+class MinimumBalanceError(Exception):
+    def __init__(self,minimum):
+        super().__init__(f"Balance cannot go below Rs.{minimum}")
+
+
+class BankAccount:
+    def __init__(self,owner,balance=0):
+        self.owner = owner 
+        self.balance = balance 
+    
+
+    def deposite(self,amount):
+        try:
+            if amount <=0:
+                raise NegativeAmountError()
+            self.balance += amount 
+            print(f"Deposited Rs.{amount}. Balance: Rs.{self.balance}")
+        except NegativeAmountError as e:
+            print(f" {e}")
+
+    def withdraw(self,amount):
+        try:
+            if amount <=0:
+                raise NegativeAmountError()
+            if amount >self.balance:
+                raise InsufficientFundsError(self.balance , amount)
+            self.balance -= amount 
+            print(f"Withdraw Rs.{amount}. Balance: Rs.{self.balance}")
+        except (NegativeAmountError,InsufficientFundsError) as e:
+            print(f"{e}")
+
+    def get_details(self):
+        print(f"Owner : {self.owner}")
+        print(f" Balance: Rs.{self.balance}")
+        print(f" Type : {type(self).__name__}")
+
+    def to_dict(self):
+        return {
+            
+        }
+
+
+
+
+
