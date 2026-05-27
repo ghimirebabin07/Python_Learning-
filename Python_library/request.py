ca624ef64dcf798f46cf8249d99ac2c7f68e6605
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-05-27 22:17:51 +0545

    Installed and Uses the request library to check the current weather

---
+++ Python_library/request.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://api.open-meteo.com/v1/forecast"
+
+params = {
+    "latitude": 27.7,   
+    "longitude": 85.3,
+    "current_weather": True
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+weather = data["current_weather"]
+print(f"Temperature : {weather['temperature']}°C")
+print(f"Wind Speed  : {weather['windspeed']} km/h")
+print(f"Weather Code: {weather['weathercode']}")
