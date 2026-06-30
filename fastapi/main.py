cb0c19c17e2bfd21213304a6807ba80866170c5c
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-06-30 23:13:53 +0545

    FastAPI Multiple Routes

--- fastapi/main.py
+++ fastapi/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def home ():
     return {"message":"Hello worl from fastapi venv"}
+
+@app.get("/About")
+def About ():
+    return {"Message":"welcome from the about page"}
+
+@app.get("/user")
+def user ():
+    return {"user":["Babin","Hari","Ram"]}
