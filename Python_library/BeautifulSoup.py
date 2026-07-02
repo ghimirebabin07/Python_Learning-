e9a8b408fda3aecad8902ec193ae058b06e587d0
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-07-02 23:06:18 +0545

    FastAPI CRUD operation create and read

--- Python_library/BeautifulSoup.py
+++ Python_library/BeautifulSoup.py
@@ -1,7 +1,7 @@
 # BeautifulSoup (bs4) is a Python library used for web scraping — it helps extract data from HTML and XML files.
 
 from bs4 import BeautifulSoup
-import requests
+import requests 
 
 url = "https://google.com"
 response = requests.get(url)
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-Learning the python 
-First = OOP 
-Second = Errors Handling 
-Third = File Handling (running):Json file 
+
 
