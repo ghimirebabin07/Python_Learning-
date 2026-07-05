1926c883b8a3fcf8b6ea470e65d22c3ca9ea3823
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-07-05 22:01:54 +0545

    code

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,114 +4,114 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# class user(BaseModel):
-#     name:str
-#     age:int
+class user(BaseModel):
+    name:str
+    age:int
 
-# @app.get("/")
-# def home ():
-#     return {"message":"Hello worl from fastapi venv"}
+@app.get("/")
+def home ():
+    return {"message":"Hello worl from fastapi venv"}
 
-# @app.get("/About")
-# def About ():
-#     return {"Message":"welcome from the about page"}
+@app.get("/About")
+def About ():
+    return {"Message":"welcome from the about page"}
 
-# @app.get("/products")
-# def get_items (limit: int =10):
-#     return{"Limits :":limit}
-
-
-# @app.get("/items")
-# def get_users(name:str=None, price:int=0):
-#     return{
-#         "Name:" :name,
-#         "Price":price
-#            }
-
-###Post request 
-# class user(BaseModel):
-#     name:str
-#     age:int
-
-# @app.post("/create-user")
-# def create_user(user:user):
-#     return{
-#         "message":"user created",
-#         "data":user
-#     }
+@app.get("/products")
+def get_items (limit: int =10):
+    return{"Limits :":limit}
 
 
-# class User(BaseModel):
-#     name:str
-#     age:int
-#     email:str
+@app.get("/items")
+def get_users(name:str=None, price:int=0):
+    return{
+        "Name:" :name,
+        "Price":price
+           }
 
-# @app.post("/create_user")
-# def create_user(user:User):
-#     return {
-#         "message":"User created",
-#         "data":user
-#     }
+##Post request 
+class user(BaseModel):
+    name:str
+    age:int
+
+@app.post("/create-user")
+def create_user(user:user):
+    return{
+        "message":"user created",
+        "data":user
+    }
+
+
+class User(BaseModel):
+    name:str
+    age:int
+    email:str
+
+@app.post("/create_user")
+def create_user(user:User):
+    return {
+        "message":"User created",
+        "data":user
+    }
     
 
-# class Address(BaseModel):
-#     city:str
-#     pincode:int
+class Address(BaseModel):
+    city:str
+    pincode:int
 
-# class User(BaseModel):
-#     name:str
-#     age:int
-#     address:Address
+class User(BaseModel):
+    name:str
+    age:int
+    address:Address
 
-# @app.post("/Create_User")
-# def Create_User(user:User):
-#     return {
-#         "Message":"User created",
-#         "data":user
-#     }
+@app.post("/Create_User")
+def Create_User(user:User):
+    return {
+        "Message":"User created",
+        "data":user
+    }
 
 
-# todos = []
-# class Todo(BaseModel):
-#     id : int
-#     title:str
-#     completed:bool
+todos = []
+class Todo(BaseModel):
+    id : int
+    title:str
+    completed:bool
 
-# @app.post("/todos")
-# def create_todo(todo:Todo):
-#     todos.append(todo)
-#     return {"Message":"todo is added","data":todo}
+@app.post("/todos")
+def create_todo(todo:Todo):
+    todos.append(todo)
+    return {"Message":"todo is added","data":todo}
 
-# @app.get("/todos")
-# def get_todos():
-#     return todos
+@app.get("/todos")
+def get_todos():
+    return todos
 
-# @app.get("/todos/{todo_id}")
-# def get_todo(todo_id:int):
-#     for todo in todos:
-#         if todo.id == todo_id:
-#             return todo
-#     return {"Error":"Todo not found "}
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {"Error":"Todo not found "}
 
-# @app.put("/todos/{todo_id}")
-# def update_todo(todo_id: int, updated_todo: Todo):
-#     for index, todo in enumerate(todos):
-#         if todo.id == todo_id:   # Assuming Todo has an 'id' field
-#             todos[index] = updated_todo
-#             return {
-#                 "Message": "Data Updated",
-#                 "Data": updated_todo
-#             }
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo: Todo):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:   # Assuming Todo has an 'id' field
+            todos[index] = updated_todo
+            return {
+                "Message": "Data Updated",
+                "Data": updated_todo
+            }
 
-#     return {"Error": "Todo not found"}
+    return {"Error": "Todo not found"}
 
-# @app.delete("/todo/{todo_id}")
-# def delete_todo(todo_id:int):
-#     for index, todo in enumerate(todos):
-#         if todo.id == todo_id:
-#             todos.pop(index)
-#             return {"message":"Data is Deleted"}
-#     return {"Error":"todo not found"}
+@app.delete("/todo/{todo_id}")
+def delete_todo(todo_id:int):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos.pop(index)
+            return {"message":"Data is Deleted"}
+    return {"Error":"todo not found"}
 
 #path query and body params 
 users = []
