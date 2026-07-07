dde65dbf5fe150183ac796ce0fc1a7ca51dc8091
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-07-07 22:49:38 +0545

    feat: add response models, status codes, custom responses, and basic error handling

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,25 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status,HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
+# class User(BaseModel):
+#     name:str
+#     age:int
+#     password:str
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
+# class UserResponce(BaseModel):
+#     name:str
+#     age:int 
 
+# @app.get("/user", response_model=UserResponce)
+# def get_user():
+#     return {
+#         "name":"Babin",
+#         "age":20,
+#             "password":"1234"    }
 
-@app.post("/items/")
-async def create_item(item: Item) -> Item:
-    return item
+@app.post("/user_created",status_code=status.HTTP_201_CREATED)
+def user_created():
+    return{
+        "message":"user created"
+    }
 
+@app.get("/user")
+def get_user():
+    return {
+        "status":"success",
+        "message":"user created",
+        "data":{
+            "name":"babin",
+            "age":"20"
 
-@app.get("/items/")
-async def read_items() -> list[Item]:
-    return [
-        Item(name="Portal Gun", price=42.0),
-        Item(name="Plumbus", price=32.0),
-    ]
+        }
+    }
+
+@app.get("/user/{user_id}")
+def get_user(user_id:int):
+    if user_id != 1:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found "
+        )
+    return{
+        "id":1,
+        "name":"babin"
+    }
