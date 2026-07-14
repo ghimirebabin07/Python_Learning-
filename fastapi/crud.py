71f884d9b1e83edd952858f9b3909c08b11d0966
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-07-14 19:15:35 +0545

    Feat: Delete Operations using fastapi in db

--- fastapi/crud.py
+++ fastapi/crud.py
@@ -79,3 +79,18 @@ def update_todo(todo_id:int,title:str,db:Session=Depends(get_db)):
         "Message":"Todo Updated",
         "data":todo
     }
+
+#delete operations 
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id:int, db:Session=Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo:
+        raise HTTPException(status_code=404,detail="todo not found")
+    
+    db.delete(todo)
+    db.commit()
+    return{
+        "message":"Todo Deleted"
+    }
