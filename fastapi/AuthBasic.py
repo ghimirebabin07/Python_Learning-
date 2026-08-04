0d41356b35ccbafb86dd70889b240e32c5096d4e
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-08-04 22:11:17 +0545

    feat: implement APIKeyQuery authentication

--- fastapi/AuthBasic.py
+++ fastapi/AuthBasic.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Header,Depends,HTTPException
 from fastapi.security import APIKeyHeader
+from fastapi.security import APIKeyQuery
 
 
 app = FastAPI()
@@ -15,7 +16,7 @@ def get_user(user=Depends(verify_token)):
         "Message":"Welcome!",
         "User":user
     }
-#API Key Authentication 
+#API Key Authentication using APIkeyheader
 API_KEY = "123sec"
 
 api_key_header =  APIKeyHeader(name="X-API-Key")
@@ -29,6 +30,25 @@ def verify_api_key(api_key: str = Depends(api_key_header)):
     return api_key
 
 @app.get("/products")
+def products(api_key=Depends(verify_api_key)):
+    return {
+        "message": "Authenticated",
+        "api_key": api_key
+    }
+
+#Using APIkeyQueries 
+
+api_key_query = APIKeyQuery(name="api_key")
+
+def verify_api_keyq(api_key: str = Depends(api_key_query)):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid API Key"
+        )
+    return api_key
+
+@app.get("/home")
 def products(api_key=Depends(verify_api_key)):
     return {
         "message": "Authenticated",
