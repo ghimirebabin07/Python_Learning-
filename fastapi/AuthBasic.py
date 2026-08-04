35c27f812759f087fbc0cff529b963768177bb2a
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-08-04 22:04:17 +0545

    feat: implement API key authentication using APIKeyHeader

--- fastapi/AuthBasic.py
+++ fastapi/AuthBasic.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI,Header,Depends,HTTPException
+from fastapi.security import APIKeyHeader
 
 
 app = FastAPI()
-
-
 def verify_token(token:str = Header()):
     if token != "mysecret":
         raise HTTPException(status_code=401,detail="Unauthorized user")
@@ -15,4 +14,23 @@ def get_user(user=Depends(verify_token)):
     return {
         "Message":"Welcome!",
         "User":user
+    }
+#API Key Authentication 
+API_KEY = "123sec"
+
+api_key_header =  APIKeyHeader(name="X-API-Key")
+
+def verify_api_key(api_key: str = Depends(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid API Key"
+        )
+    return api_key
+
+@app.get("/products")
+def products(api_key=Depends(verify_api_key)):
+    return {
+        "message": "Authenticated",
+        "api_key": api_key
     }
