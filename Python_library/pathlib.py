fbb4f124966a6a8e9e50b8636bfa5d7d382856d6
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-08-10 20:45:28 +0545

    learn pathlib file existence check

--- Python_library/pathlib.py
+++ Python_library/pathlib.py
@@ -5,3 +5,12 @@ file = Path("hello.txt")
 file.write_text("Hello, I am learning Python!")
 
 print(file.read_text()) 
+
+#program 2 
+
+file = Path("hello.txt")
+
+if file.exists():
+    print("File exists")
+else:
+    print("File does not exist")
