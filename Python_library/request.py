5825d49a6238b86b20f8eb76882e59f14ec582f3
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-08-11 14:28:31 +0545

    fix: resolve pathlib module conflict and add GET JSON example

--- Python_library/request.py
+++ Python_library/request.py
@@ -5,3 +5,13 @@ response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 print(response)
 print(response.status_code)
 print(response.text) 
+
+#Get JSON data 
+response = requests.get(
+    "https://jsonplaceholder.typicode.com/posts/1"
+)
+
+data = response.json()
+
+print(data)
+print(data["title"])
