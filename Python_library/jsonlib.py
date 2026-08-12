d9cb2dee6add840040c967f5f0917cf0ec41daec
ghimirebabin07 <216760506+ghimirebabin07@users.noreply.github.com>
2026-08-12 15:37:44 +0545

    jsonlib python new libaries basic example

--- Python_library/jsonlib.py
+++ Python_library/jsonlib.py
@@ -9,3 +9,11 @@ data = {
 json_data = json.dumps(data)
 
 print(json_data) 
+
+#json.loads() 
+son_data = '{"name": "Babin", "age": 20}'
+
+data = json.loads(json_data)
+
+print(data)
+print(data["name"]) 
